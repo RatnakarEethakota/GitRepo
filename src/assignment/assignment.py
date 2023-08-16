71f671aa01e440353208b56015bc9b0b9356387c
unknown <ratnakar.eethakota@diggibyte.com>
2023-08-16 15:48:08 +0530

    made some changes in .py file

--- src/assignment/assignment.py
+++ src/assignment/assignment.py
@@ -1,14 +1,23 @@
-n = 10
-num1 = 0
-num2 = 1
-next_number = num2
-count = 1
+# Python program to check if difference between sum of
+# odd digits and sum of even digits is 0 or not
 
-while count <= n:
-	print(next_number, end=" ")
-	count += 1
-	num1, num2 = num2, next_number
-	next_number = num1 + num2
-print()
+# Reading Input
+num = int(input())
+string1 = str(num)
+evensum = 0
+oddsum = 0
 
-print(yes)
+# Driver Code
+for i in range(0, len(string1)):
+    if (i % 2 == 0):
+        evensum += int(string1[i])
+    else:
+        oddsum += int(string1[i])
+
+# Condition
+if (oddsum - evensum == 0):
+    print("Yes")
+else:
+    print("No")
+
+print("ok")
